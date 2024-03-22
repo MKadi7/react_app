4551ddc5246ee8ee9f97c7e4c54fc5d66246d31e
Mkadi7 <gadimuhammad.7@gmail.com>
2024-03-21 22:30:49 -0400

    sprint 2 commit

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './MainMenu.scss'; // Import your SCSS file for styling
 
 const MainMenu = () => {
   return (
-    <nav>
+    <nav className="main-menu">
       <ul>
         <li>
-          <Link to="/">Tasks</Link>
+          <Link to="/" className="menu-link">Tasks</Link>
         </li>
         <li>
-          <Link to="/add">Add</Link>
+          <Link to="/add" className="menu-link">Add</Link>
         </li>
         <li>
-          <Link to="/help">Help</Link>
+          <Link to="/help" className="menu-link">Help</Link>
         </li>
       </ul>
     </nav>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 function NotFound() {
   return (
     <div>
-      <h2>404 - Page Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
+      <h2>TaskEase</h2>
+      <p> Welcome to TaskEase </p>
     </div>
   );
 }
